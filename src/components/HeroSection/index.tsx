b0a74398db59b3ca758730cd88dc81201fdca2e6
fomes <fgomesdeluna@gmail.com>
2023-10-27 15:38:33 -0300

    Hero section

---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { TypeAnimation } from "react-type-animation";
+
+export function HeroSection() {
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12 xs:flex flex-col">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600">
+              Hello, I'm Felipe
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={["Web Developer", 1000, "Full Stack Developer", 1000]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl sm:mr-4">
+            Desenvolvedor Full Stack especialista em React.
+          </p>
+          <div>
+            <Link
+              href="/contact"
+              className="px-6 inline-block py-3 w-full font-medium sm:w-fit rounded-full mr-4 bg-gradient-to-br from-purple-500 to-blue-500 hover:bg-slate-200 hover:bg-gradient-radial text-white hover:text-black transition-all duration-300"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="/"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-500 to-blue-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] font-medium hover:bg-blue-500 rounded-full px-5 py-2 transition-all duration-300">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/assets/hero.png"
+              alt="hero image"
+              className="absolute rounded-full transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={400}
+              height={400}
+            />
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+}
