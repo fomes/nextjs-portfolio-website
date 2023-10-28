e122e3690a2cf25eee92650194e20e2ec6b3ad53
fomes <fgomesdeluna@gmail.com>
2023-10-27 22:05:28 -0300

    About section

---
+++ _data/db.tsx
@@ -0,0 +1,36 @@
+export const userData = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>Nextjs</li>
+        <li>Prisma</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Fullstack Trybe Course</li>
+        <li>Analysis and Systems Development</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Experience",
+    id: "experiece",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
